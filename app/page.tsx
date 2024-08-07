e2b2179ebf28f00f46b051480a018c6a657f7ddc
Ameer Hamza <ameer.hamza102002@gmail.com>
2024-08-07 23:28:17 +0500

    Branch added for subhan

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <h1>Hello World!</h1>
+     <h1>Welcome Subhan! </h1>
+     <p>This is the branch associated to you, you can push the code here and then can create the pull request to merge into the main</p>
     </main>
   );
 }
