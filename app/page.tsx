c4a77bde15e0b89ee7f55a974ebbdafe49c6992c
aaakash06 <akashbagale182@gmail.com>
2024-08-11 22:39:45 +0545

    llama3 70B added

--- app/page.tsx
+++ app/page.tsx
@@ -1,44 +1,45 @@
-'use client'
-import { useState } from 'react';
-import Head from 'next/head';
-import ChatContainer from '@/components/ChatContainer';
-import ChatMessage from '@/components/ChatMessage';
-import { FaUserCircle } from 'react-icons/fa';
-import { ChatResponse, Message } from '@/types/page';
-
+"use client";
+import { useState } from "react";
+import Head from "next/head";
+import ChatContainer from "@/components/ChatContainer";
+import ChatMessage from "@/components/ChatMessage";
+import { FaUserCircle } from "react-icons/fa";
+import { ChatResponse, Message } from "@/types/page";
 
 export default function Home() {
   const [messages, setMessages] = useState<Message[]>([
-    { text: 'How are you?', sender: 'bot' },
-    { text: 'Write me an essay about ancient Rome, their civilization and culture and what food was the best considered item among them?', sender: 'user' },
-    { text: 'How are you?', sender: 'bot' },
+    { text: "How are you?", sender: "bot" },
+    {
+      text: "Write me an essay about ancient Rome, their civilization and culture and what food was the best considered item among them?",
+      sender: "user",
+    },
+    { text: "How are you?", sender: "bot" },
   ]);
 
   const [dropdownOpen, setDropdownOpen] = useState<boolean>(false);
 
-
   async function sendMessage(message: string): Promise<string> {
-    const response = await fetch('/api/chat', {
-      method: 'POST',
+    const response = await fetch("/api/chat", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({ message }),
     });
-
     const data: ChatResponse = await response.json();
+
     return data.message;
   }
   const handleSendMessage = async (text: string) => {
-    setMessages([...messages, { text, sender: 'user' }]);
-    const aiResponse = await sendMessage(text);
-    console.log(aiResponse, "response");
-
+    setMessages([...messages, { text, sender: "user" }]);
+    const aiResponse: string = await sendMessage(text);
+    // the above set message is not added for some reason
+    setMessages([...messages, { text: aiResponse, sender: "bot" }]);
   };
 
   const handleLogout = () => {
     // Implement logout functionality here
-    console.log('Logged out');
+    console.log("Logged out");
   };
 
   return (
@@ -71,7 +72,11 @@ export default function Home() {
         <h2>if you have any query you can ask freely </h2>
         <ChatContainer onSendMessage={handleSendMessage}>
           {messages.map((message, index) => (
-            <ChatMessage key={index} text={message.text} sender={message.sender} />
+            <ChatMessage
+              key={index}
+              text={message.text}
+              sender={message.sender}
+            />
           ))}
         </ChatContainer>
       </main>
--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -1,40 +1,45 @@
-import  OpenAI  from "openai";
 import type { NextApiRequest, NextApiResponse } from "next";
+const keyGroq = process.env.keyGroq;
 
-const configuration = {
-  apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
-};
+import Groq from "groq-sdk";
 
-const openai = new OpenAI(configuration);
+const groq = new Groq({ apiKey: keyGroq });
+
+export async function getGroqChatCompletion() {
+  return groq.chat.completions.create({
+    messages: [
+      {
+        role: "user",
+        content: "tell me your model name",
+      },
+    ],
+    model: "llama3-70b-8192",
+  });
+}
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method !== 'POST') {
+  console.log("api is hit");
+  if (req.method !== "POST") {
+    console.log("Not post request");
 
-    console.log('Not post request');
-    
     return res.status(405).json({ message: "Only POST requests are allowed" });
   }
   const { message } = req.body;
   if (!message) {
-    console.log('Message is required');
-    
+    console.log("Message is required");
+
     return res.status(400).json({ message: "Message is required" });
   }
   try {
-    const response = await openai.chat.completions.create({
-      model: "gpt-3.5-turbo",
-      messages: [{ role: "user", content: message }],
-    });
-
-    const completion = response.choices[0].message?.content;
-
+    const chatCompletion = await getGroqChatCompletion();
+    const completion = chatCompletion.choices[0]?.message?.content || "";
     res.status(200).json({ message: completion });
   } catch (error: any) {
-    console.log("Error",error);
-    
+    console.log("Error", error);
+
     res
       .status(500)
       .json({ message: "Error with OpenAI API", error: error.message });
